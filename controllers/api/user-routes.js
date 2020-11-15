e2662e0ae503abe4c747925d4f1166360e9c09ac
Nicole Gile <nicolegile94@gmail.com>
2020-11-14 19:04:19 -0600

    sd

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -62,7 +62,15 @@ router.post('/', (req, res) => {
         email: req.body.email,
         password: req.body.password
     })
-    .then(dbUserData => res.json(dbUserData))
+    .then(dbUserData => {
+        req.session.save(() => {
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+
+            res.json(dbUserData);
+        });
+    })
     .catch(err => {
         console.log(err);
         res.status(500).json(err);
@@ -86,6 +94,12 @@ router.post('/login', (req, res) => {
             res.status(400).json({ message: 'Incorrect Password! '});
             return;
         }
+
+        req.session.save(() => {
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+        })
 
         res.json({user: dbUserData, message: 'You are now logged in!' });
     });
@@ -133,6 +147,16 @@ router.delete('/:id', (req, res) => {
         console.log(err);
         res.status(500).json(err);
     });
+});
+
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
+    }
 });
 
 module.exports = router;
